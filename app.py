8cc8c1d2e5a4117149caa03a4d2c78feac65ddd9
Zissi Milstein <126404910+Zissi-Milstein@users.noreply.github.com>
2024-07-16 15:49:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -94,10 +94,10 @@ def clone_voice(text_input, uploaded_file):
     st.text('Synthesizing...')
     # synthesized_audio = tts(text_input)[0]['audio']
     # st.audio(synthesized_audio, format='audio/wav')
-    try:
-        tts.tts_to_file(text=text_input, speaker_wav=[uploaded_file,uploaded_file], language="en", file_path="./output.wav")
+    # try:
+    tts.tts_to_file(text=text_input, speaker_wav=[uploaded_file,uploaded_file], language="en", file_path="./output.wav")
         # return "./output.wav"
-    except Exception as e:
+    # except Exception as e:
         # st.error(f"Error synthesizing voice: {e}")
 
 
