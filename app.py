66f2628e485e479db960f0ed3a1c3b066dae5dc6
Zissi Milstein <126404910+Zissi-Milstein@users.noreply.github.com>
2024-07-16 08:51:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ tts = None
 
 try:
     # tts = TTS(model_dir)
-    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
     st.success("Coqui TTS model loaded successfully!")
 except Exception as e:
     st.error(f"Error loading Coqui TTS model: {e}")
